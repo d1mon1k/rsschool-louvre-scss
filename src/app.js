4c1c878d6a1e2265ea8e2c3ee9457bb7fe808971
Artyom <ostrovskiy.a.97@gmail.com>
2021-08-08 20:38:21 +0300

    init: start museum task

---
+++ src/app.js
@@ -0,0 +1,4 @@
+require('./index.html');
+import 'normalize.css';
+import './scss/base.scss';
+import './scss/header.scss';
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path'); //[ru] - требовать, это глобальная функция Node.js
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack') //?
+// const autoprefixer = require('autoprefixer') //?
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: "./src/app.js",
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'), //__dirname - Это абсолютный путь к нашему js файлу в каталоге. path.resolve([путь]) - используется для преобразования последовательности сегментов в абсолютный путь => текущий путь к repo/dist/bundle.js
+    filename: 'bundle.js'
+  },
+  module: { //В данном модуле прописываются правила лоадеров и плагинов.
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          MiniCssExtractPlugin.loader, //По сути miniCss.. заменяет собой 'style-loader'  Когда minicss.. не сможет выполнить работу будет применён fallback.
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: './src/scss/vars.scss',
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "raw-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack Boilerplate',
+      template: path.resolve(__dirname, './src/index.html'), // шаблон
+      filename: 'index.html', // название выходного файла
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.LoaderOptionsPlugin({ // ?
+    //   options: {
+    //     postcss: [
+    //       autoprefixer()
+    //     ]
+    //   }
+    // })
+  ]
+};
+
